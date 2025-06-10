99fdf2fd93597c0aca1a29c387d6b1973bb78056
maixuanhieu135 <111688465+maixuanhieu135@users.noreply.github.com>
2025-06-10 14:27:52 +0700

    fix history result

--- src/services/test.service.js
+++ src/services/test.service.js
@@ -158,7 +158,8 @@ const getTestResult = async ({ testResultId }) => {
 const getTestHistory = async ({ userId, testId }) => {
   const testResults = await TestResult.find({ userId, testId })
     .populate("testId")
-    .sort({ createdAt: -1 });
+    .sort({ createdAt: -1 })
+    .select('testId startIsoDate createdAt grade');
   return testResults;
 };
 module.exports = {
